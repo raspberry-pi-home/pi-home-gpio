250fe130dc5c0c0f717aab09b15d36467ee10f25
Gustavo Ingrassia <gustavo.ingrassia@gmail.com>
2020-05-29 11:58:16 -0300

    remove test

--- src/lib.test.tsx
+++
@@ -1,6 +0,0 @@
-const { Led, Button } = require('../lib/pi-home-gpio')
-
-test('basic instantiation', () => {
-  expect(new Led(1)).not.toBeUndefined()
-  expect(new Button(1)).not.toBeUndefined()
-})
